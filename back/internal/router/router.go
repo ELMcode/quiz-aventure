be87b632f21aadcc3d3eabba07c65582c9694727
amine.elalame <elm.dev.code@gmail.com>
2024-08-02 14:21:34 +0200

    feat (back/internal/router): Create a router with Chi package, implement CORS Authorizations to enable front/back communication & init '/questions' and '/check-answer' routes

---
+++ back/internal/router/router.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+
+	"quiz-aventure/internal/config"
+	"quiz-aventure/internal/handlers"
+)
+
+func NewRouter(cfg *config.Config) *chi.Mux {
+	r := chi.NewRouter()
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   cfg.CORS.AllowedOrigins,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+	r.Use(c.Handler)
+
+	r.Get("/questions", handlers.GetQuestions)
+	r.Post("/check-answer", handlers.CheckAnswer)
+
+	return r
+}
